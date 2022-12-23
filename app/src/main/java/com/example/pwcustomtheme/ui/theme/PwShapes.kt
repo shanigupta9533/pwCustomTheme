46a2d93c19d77640eb0490cd7e85d5ed2de5d829
shani gupta <shani.gupta@pw.live>
2022-12-23 19:37:45 +0530

    pwshapes and pwtheme attributes changes

--- app/src/main/java/com/example/pwcustomtheme/ui/theme/PwShapes.kt
+++ app/src/main/java/com/example/pwcustomtheme/ui/theme/PwShapes.kt
@@ -4,9 +4,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.staticCompositionLocalOf
 
 data class PwShapes(
-    val mediumShapes: RoundedCornerShape = RoundedCornerShape(50),
-    val circleShape: RoundedCornerShape = RoundedCornerShape(100),
-    val minimumShape: RoundedCornerShape = RoundedCornerShape(35)
+    val mediumShapes: RoundedCornerShape = RoundedCornerShape(percent = 50),
+    val circleShape: RoundedCornerShape = RoundedCornerShape(percent = 100),
+    val minimumShape: RoundedCornerShape = RoundedCornerShape(percent = 35)
 )
 
 internal val LocalShapes = staticCompositionLocalOf { PwShapes() }
